aae658ab926c09ff93ae75515fcec697071ff7cd
Wilham G. L. Von Dër Brodwolf <wilham@brodwolf.dev>
2023-10-03 00:12:35 -0300

    initialize Nextjs and PWA

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export const Head = () => {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      <link rel="manifest" href="/manifest.json" />;
+    </>
+  );
+};
